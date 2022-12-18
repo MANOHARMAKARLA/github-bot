b56a3f1865af1e220b0d518c19724a7431cd132c
manoharmakarla <MANOHAR.MNS04@GMAIL.COM>
2022-12-18 18:38:43 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,11 +2,18 @@ const jsonfile = require('jsonfile');
 const moment = require('moment');
  const simpleGit =  require('simple-git'); 
 const FILE_PATH = './data.json';
-const DATE  = moment().format();
-const data = {
-  date: DATE
+const makecommit = (x)=>{
+  const DATE  = moment().subtract(1,'y').add(1,'d')
+                  .add(x,'d').format();
+  const data = {
+    date: DATE
+  }
+  jsonfile.writeFile(FILE_PATH, data,()=>{
+    simpleGit().add(FILE_PATH).commit (DATE,{'--date': DATE }).push();
+  
+  });
+  
 }
-jsonfile.writeFile(FILE_PATH, data);
-
-simpleGit().add([FILE_PATH1]).commit (DATE,{'--date': DATE }).push();
-                                                      
+for (let i = 0; i < 365; i++) {
+makecommit(i);
+}
