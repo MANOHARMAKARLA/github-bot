3b7160576a11475e8406ee6495e7547d58f55798
manoharmakarla <MANOHAR.MNS04@GMAIL.COM>
2022-12-18 19:50:19 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,18 +2,29 @@ const jsonfile = require('jsonfile');
 const moment = require('moment');
  const simpleGit =  require('simple-git'); 
 const FILE_PATH = './data.json';
-const makecommit = (x)=>{
+
+const random  = require("random-number-csprng");
+
+
+
+const makecommit = (n)=>{
+
+  if(n===0) return 0;
+  simpleGit().push();
+  const x = random(0,54);
+  const y = random(0,6);
   const DATE  = moment().subtract(1,'y').add(1,'d')
-                  .add(x,'d').format();
+                  .add(x,'w').add(y,'d').format();
   const data = {
     date: DATE
   }
+  console.log(data);
   jsonfile.writeFile(FILE_PATH, data,()=>{
-    simpleGit().add(FILE_PATH).commit (DATE,{'--date': DATE }).push();
+
+    simpleGit().add(FILE_PATH).commit (DATE,{'--date': DATE },makecommit.bind(this,--n));
   
   });
   
 }
-for (let i = 0; i < 365; i++) {
-makecommit(i);
-}
+ 
+makecommit(10000);
